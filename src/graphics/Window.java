473651738bb24a3faa8ebf8676de8653d5b011e2
EdanSneh <edan.sneh@gmail.com>
2016-11-11 12:02:44 -0800

    Added init to Window

--- src/graphics/Window.java
+++ src/graphics/Window.java
@@ -1,5 +1,7 @@
 package graphics;
 
+import java.awt.Color;
+
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
@@ -7,8 +9,17 @@ public class Window extends JFrame{
 	
 	JComponent screen;
 	
+	public void init(){
+		Color back = new Color(0x484446);
+		this.setBackground(back);
+		this.setSize(800, 800);
+		this.repaint();
+	}
+	
 	public Window(JComponent screen){
+		init();
 		this.screen = screen;
 		this.setVisible(true);
+		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 	}
 }
