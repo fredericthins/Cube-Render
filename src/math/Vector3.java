818b6a53a7f54466388dbdb105cadfa622aee622
Frederic Maa <fredericthins@gmail.com>
2016-11-11 14:52:05 -0800

    Documentation chagnes

--- src/math/Vector3.java
+++ src/math/Vector3.java
@@ -19,82 +19,177 @@ package math;
  * 		Project(Vector3, Vector3)
  */
 public class Vector3{
-	float x; 
-	float y;
-	float z;
-	public Vector3(Matrix m){  						  //Vector3 constructor using a Matrix
+	float x; //X coordinate
+	float y; //Y coordinate
+	float z; //Z coordinate
+	/* Creates a new 3D Vector with a given Matrix as the coordinates
+	 * 
+	 * @param Matrix the Matrix of coordinates and scalar
+	 */
+	public Vector3(Matrix m){  						
 		float lastValue = m.getValue(0, 3);
 		this.x = m.getValue(0,0) / lastValue;
 		this.y = m.getValue(0,1) / lastValue;
 		this.z = m.getValue(0,2) / lastValue;
 	}
-	public Vector3(float x, float y, float z){		 //Vector3 constructor using three float coordinates
+	/* Creates a new 3D Vector given the coordinates
+	 * 
+	 * @param x the float that represents the x coordinate
+	 * @param y the float that represents the y coordinate
+	 * @param z the float that represents the z coordinate
+	 */
+	public Vector3(float x, float y, float z){	
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
-	public Vector3 clone(){							//Copy the current vector and return the copy Vector3
-		Vector3 newVector = new Vector3(getX(),getY(),getZ());
-		return newVector;
+	/* Creates a copy of the 3D vector
+	 * 
+	 * @return newVector the copy of the current vector
+	 */
+	public Vector3 clone(){
+		Vector3 newVector = new Vector3(getX(),getY(),getZ()); //Creates the copy of Vector
+		return newVector;									//Returns the copy version of Vector
 	}
-	public String toString(){						//Return a string that tells the components of Vector3
-		String ans = new String("X = " + getX() + ", Y = " + getY() + ", Z = " + getZ());
-		return ans;
+	/* Return a string that shows attributes of a vector
+	 * 
+	 * @return str the string that shows the attributes of the vector
+	 */
+	public String toString(){						
+		String str = new String("X = " + getX() + ", Y = " + getY() + ", Z = " + getZ());//Creating a string that shows the attributes of a vector
+		return str;// Return the string
 	}
-	public float getMagn(){							//Gets magnitude of the vector
-		return (float) Math.sqrt(Math.pow(getX(),2) + Math.pow((getY()),2) + Math.pow(getZ(),2));
+	/* Returns the magnitude of a vector
+	 * 
+	 * @return float that represents the magnitude of the vector
+	 */
+	public float getMagn(){
+		return (float) Math.sqrt(Math.pow(getX(),2) + Math.pow((getY()),2) + Math.pow(getZ(),2)); //Return the magnitude
 	}
-	public Vector3 scale(float scalar){				//Scale a vector by a magnitude of scalar
-		this.setX(getX()*scalar);
-		this.setY(getY()*scalar);
-		this.setZ(getZ()*scalar);
-		return this;
+	/* Returns a vector that is scaled by a value of scalar
+	 * 
+	 * @param scalar the float that represents the scale
+	 * 
+	 * @return the vector that is scaled by value scalar
+	 */
+	public Vector3 scale(float scalar){
+		this.setX(getX()*scalar); //Scaling the x component by value scalar
+		this.setY(getY()*scalar); //Scaling the y component by value scalar
+		this.setZ(getZ()*scalar); //Scaling the z component by value scalar
+		return this; //Return the Vector3 scaled to value scalar
 	}
-	public Vector3 normalize(){						//Return a normalized version of the current vector3
+	/*Return a nomarlized version of the current Vector3
+	 * 
+	 * @return normalized version of the current vector
+	 */
+	public Vector3 normalize(){	
 		return scale(1/Math.abs(getMagn()));
 	}
-	public Matrix homogeneous(){					//Return the homogeneous of the current vector [1][4] with the last value as 1
-		float[][] tempMatrix = {{this.getX()},{this.getY()},{this.getZ()}};   
-		Matrix homoVec = new Matrix(tempMatrix);
-		return homoVec;
+	/*Return homogeneous version of the vector
+	 * 
+	 * @return homoVec the Matrix[1][4] that gives the coordinates of the vector and scalar
+	 */
+	public Matrix homogeneous(){					
+		float[][] tempMatrix = {{this.getX()},{this.getY()},{this.getZ()}}; //Creating a matrix with the coordinates of vector  
+		Matrix homoVec = new Matrix(tempMatrix);//Translate into a matrix
+		return homoVec;//Return the matrix
 	}
-	public Vector3 rotate(Quaterion){				//Rotating a vector
-		Quaterion q1 =  new Quaterion(0, this);
-		Quaterion q2 = q.norm();
-		Quaterion q3 = new Quaterion(Matrix.multiply(q1.MatRot(), q2.Mat()));
-		return 
+	/*Returns a vector that is rotated by quaternion q4
+	 * 
+	 * @param q4 Quaternion that represents the rotation
+	 * 
+	 * @returns tempV the vector that has been rotated by q4
+	 */
+	public Vector3 rotate(Quaternion q4){
+		Quaternion q1 =  new Quaternion(0, this); //Quaternion representation of the current vector
+		Quaternion q2 = q4.norm(); //normalized version of quaternion q4
+		Quaternion q3 = new Quaternion(Matrix.multiply(q1.matRot(), q2.mat()));//Rotation of the vector by q4
+		Vector3 tempV = new Vector3(q3.getX(),q3.getY(),q3.getZ()); //the resulting vector rotated
+		return tempV;//returns the vector rotated
 	}
-													//Getters and Setters
+	//Getters and Setters
+	/* Returns the value of x coordinate
+	 * 
+	 * @returns x the value of x coordinate
+	 */
 	public float getX() {
 		return x;
 	}
+	/* Set a new value for the x coordinate
+	 * 
+	 * @param x float that represent the new value wanted
+	 */
 	public void setX(float x) {
 		this.x = x;
 	}
+	/* Returns the value of y coordinate
+	 * 
+	 * @returns y the value of y coordinate
+	 */
 	public float getY() {
 		return y;
 	}
+	/* Set a new value for the y coordinate
+	 * 
+	 * @param y float that represent the new value wanted
+	 */
 	public void setY(float y) {
 		this.y = y;
 	}
+	/* Returns the value of z coordinate
+	 * 
+	 * @returns z the value of z coordinate
+	 */
 	public float getZ() {
 		return z;
 	}
+	/* Set a new value for the z coordinate
+	 * 
+	 * @param z float that represent the new value wanted
+	 */
 	public void setZ(float z) {
 		this.z = z;
 	}
-public static Vector3 Add(Vector3 v1, Vector3 v2){	//The addition of two vector3(v1,v2) returns the resulting vector
+/* Return the resulting Vector of the addition of two vectors
+ * 
+ * @param v1 the first Vector
+ * @param v2 the second Vector
+ * 
+ * @return newVector the resulting vector from the addition of v1 and v2
+ */
+public static Vector3 Add(Vector3 v1, Vector3 v2){
 	Vector3 newVector = new Vector3(v1.getX() + v2.getX(),v1.getY() + v2.getY(),v1.getZ()+v2.getZ());
 	return newVector;
 }
-public static float Dot(Vector3 v1, Vector3 v2){	//Returns the dot product of two vectors
+/* Return the resulting Vector of the dot product of two vectors
+ * 
+ * @param v1 the first Vector
+ * @param v2 the second Vector
+ * 
+ * @return newVector the resulting vector from the dot product of v1 and v2
+ */
+public static float Dot(Vector3 v1, Vector3 v2){
 	return v1.getX() * v2.getX() + v1.getY() * v2.getY() + v1.getZ() * v2.getZ();
 }
-public static Vector3 cross(Vector3 v1, Vector3 v2){//Returns the cross product of two vectors
+/* Return the resulting Vector of the cross product of two vectors
+ * 
+ * @param v1 the first Vector
+ * @param v2 the second Vector
+ * 
+ * @return newVector the resulting vector from the cross product of v1 and v2
+ */
+public static Vector3 cross(Vector3 v1, Vector3 v2){
 	Vector3 newVector = new Vector3(v1.getY()*v2.getZ() - v1.getZ()*v2.getY(),v1.getZ()*v2.getX() - v1.getX()*v2.getZ(),v1.getX()*v2.getY() - v1.getY()*v2.getX());
 	return newVector;
 }
-public static Vector3 Project(Vector3 v1, Vector3 v2){//Returns the projection of two vectors(v2 is being projected onto v1)
+/* Return the resulting Vector of the projection of one vector onto another
+ * 
+ * @param v1 the first Vector
+ * @param v2 the second Vector
+ * 
+ * @return projection the resulting vector that is the projection of v2 on v1
+ */
+public static Vector3 Project(Vector3 v1, Vector3 v2){
 	Vector3 projection = v1.scale(Dot(v1,v2)/v1.getMagn());
 	return projection;
 }
