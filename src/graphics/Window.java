685ce68da61f83af72f55e605f1c1baeae1f710e
EdanSneh <edan.sneh@gmail.com>
2016-11-11 12:10:21 -0800

    added update to window

--- src/graphics/Window.java
+++ src/graphics/Window.java
@@ -1,5 +1,6 @@
 package graphics;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JComponent;
@@ -9,17 +10,26 @@ public class Window extends JFrame{
 	
 	JComponent screen;
 	
-	public void init(){
+	/**
+	 * initialize the window with certain parameter
+	 */
+	private void init(){
 		Color back = new Color(0x484446);
 		this.setBackground(back);
 		this.setSize(800, 800);
 		this.repaint();
+		
 	}
 	
 	public Window(JComponent screen){
 		init();
 		this.screen = screen;
+		this.add(this.screen, BorderLayout.CENTER);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+	
+	}
+	public void Update(){
+		
 	}
 }
