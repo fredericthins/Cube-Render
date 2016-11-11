b56bc646a49064c97026cddd57bcdf121fce6b49
Michael Chau <inventormc@gmail.com>
2016-11-11 12:12:36 -0800

    Updater class created

--- src/core/Updater.java
+++ src/core/Updater.java
@@ -1,5 +1,18 @@
 package core;
 
-public class Updater {
+public class Updater implements Runnable{
 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try{
+			while(true){
+				Thread.sleep(16);
+				Run.window.Update();;
+			}
+		}catch(InterruptedException ex){
+			System.out.println("Something went wrong: update thread interrupted");
+		}
+	}
+	
 }
--- src/graphics/Screen.java
+++ src/graphics/Screen.java
@@ -5,9 +5,14 @@ import java.awt.Graphics;
 import javax.swing.JComponent;
 
 public class Screen extends JComponent{
-	
+
 	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see javax.swing.JComponent#paint(java.awt.Graphics)
+	 */
 	public void paint(Graphics g){
 		
 	}
+	
 }
